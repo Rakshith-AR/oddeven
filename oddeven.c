513c435b8a91f56247982d8edc96f55b925e3d49
rakshith <rakshith.r@s.amity.edu>
2025-03-04 12:00:27 +0530

    added print yes and no

---
+++ oddeven.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input from user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+              print("yes")
+    // Check if the number is even or odd
+    if (num % 2 == 0) {
+        printf("%d is even.\n"not odd num);
+    } else {
+        printf("%d is odd.\n" not even, num);
+    }
+        print("no")
+    return 0;
+}
+
